02bbdd791fd42ff47a2eb3da0e2eabe2fff6ff7f
Renato Rocha Rodrigues <renato.rodrigues@findes.org.br>
2023-08-02 11:12:55 -0300

    mudanca de background

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function toggleMode() {
   const html = document.documentElement;
   html.classList.toggle("light"); //tem a msm funcionalidade do codigo a cima
 
-  const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img") // armazena a foto
 
   if (html.classList.contains("light")) {
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('src', '#')  //foto do modo light
+    img.setAttribute('alt', 'Foto de perfil modo light')
 } else {
-    img.setAttribute('src', 'https://github.com/renatorrocha.png')
+    img.setAttribute('src', 'https://github.com/renatorrocha.png') //foto do modo dark
+    img.setAttribute('alt', 'Foto de perfil modo dark')
   }
 }
