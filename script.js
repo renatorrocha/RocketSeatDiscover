fc177b2dc229e9f81e563385187c850e6919cc70
Renato Rocha Rodrigues <renato.rodrigues@findes.org.br>
2023-08-02 11:39:52 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function toggleMode() {
   const html = document.documentElement;
-  html.classList.toggle("light"); //tem a msm funcionalidade do codigo a cima
+  html.classList.toggle("light");
+  //tem a msm funcionalidade do codigo a cima
 
-  const img = document.querySelector("#profile img") // armazena a foto
+  const img = document.querySelector("#profile img"); // armazena a foto
 
   if (html.classList.contains("light")) {
-    img.setAttribute('src', '#')  //foto do modo light
-    img.setAttribute('alt', 'Foto de perfil modo light')
-} else {
-    img.setAttribute('src', 'https://github.com/renatorrocha.png') //foto do modo dark
-    img.setAttribute('alt', 'Foto de perfil modo dark')
+    img.setAttribute("src", "#"); //foto do modo light
+    img.setAttribute("alt", "Foto de perfil modo light");
+  } else {
+    img.setAttribute("src", "https://github.com/renatorrocha.png"); //foto do modo dark
+    img.setAttribute("alt", "Foto de perfil modo dark");
   }
 }
